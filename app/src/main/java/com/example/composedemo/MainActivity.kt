9fcb9f37c1933286ab760a8a21931d4f6306b461
sandip <sandip@588>
2021-04-06 19:08:39 +0530

    initial commit 2021-04-10

--- app/src/main/java/com/example/composedemo/MainActivity.kt
+++ app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -3,11 +3,21 @@ package com.example.composedemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composedemo.ui.theme.ComposeDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +27,7 @@ class MainActivity : ComponentActivity() {
             ComposeDemoTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    setLayoutUI()
                 }
             }
         }
@@ -25,14 +35,39 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun setLayoutUI() {
+    Column(modifier = Modifier.padding(10.dp)) {
+        Image(
+            painter = painterResource(R.drawable.header), contentDescription = null,
+            modifier = Modifier
+                .height(180.dp)
+                .fillMaxWidth()
+                .clip(shape = RoundedCornerShape(10.dp)),
+            contentScale = ContentScale.Crop
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        Text(
+            text = "A day wandering through the sandhills in Shark Fin Cove, and a few of the sights I saw",
+            style = typography.h6,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(
+            "Davenport, California",
+            style = typography.body2
+        )
+        Text(
+            "December 2018",
+            style = typography.body2
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeDemoTheme {
-        Greeting("Android")
+        setLayoutUI()
     }
 }
